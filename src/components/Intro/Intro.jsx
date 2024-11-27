921ef92968c0dd7360b8b81d16447cd0eb5ee33a
Harishnirmal <harishnirmal.3@gmail.com>
2024-11-27 20:45:05 +0530

    'new-f'

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
             <span className='hello'>Hello</span>
             <span className="introText">I'm <span className="introName">Harish</span><br />Frontend Developer</span>
             <p className='introPara'>I am a skilled web designer with experience in creating <br /> visually appealing and user friendly websites.</p>
-            <a href={resume} download="My_resume.pdf"><button className='btn'><img src={btnImg} alt="" className='btnImg'/><b> CV Download </b></button></a>
+            <a href={resume} download="My_resume.pdf"><button className='btn'><img src={btnImg} alt="" className='btnImg'/><b> Dowload CV</b></button></a>
         </div>
         <img src={bg} alt="profile" className='bg'/>
     </section>
